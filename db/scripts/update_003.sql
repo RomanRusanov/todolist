d90dd845cec09b75bd954013848533f74f3be81d
RomanRusanov <roman9628@gmail.com>
2021-04-26 12:32:12 +0300

    Add postgres to travis config

--- db/scripts/update_003.sql
+++ db/scripts/update_003.sql
@@ -7,3 +7,14 @@ create table j_user (
                         name varchar(2000),
                         role_id int not null references j_role(id)
 );
+create table j_role_j_user
+(
+    role_id  integer not null
+        constraint fkd0x2yn3hbl6hdhop7ddxy53lv
+            references j_role,
+    users_id integer not null
+        constraint uk_eynvk8o4fq3ouuclffj2an8i5
+            unique
+        constraint fk8u2html5ih2fk6n6fnjteyvdi
+            references j_user
+);
